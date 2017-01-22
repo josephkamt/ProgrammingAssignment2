de9c58b93da05da25f03acd1e2412d819673f1fe
josephkamt <josephkamt@Josephs-MacBook-Pro.local>
2017-01-22 18:39:41 -0500

    Assigment 2 completed

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a list of set() and get()
+## Funtion creates the environment of data where chacheSolve will feed for
+## solution
 
-## Write a short comment describing this function
+## Sets the class of the values of x
+## creates a list where to extract data
+## to get inversem, the matrix has to be squared in dimensions
+## NULL vector generated if this condition is not met
+## print of warning messages
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        set <- function(y) {
+                x <<- y
+        }
+        if(nrow(x) != ncol(x)) {
+                message("matrix is not squared")
+                message("matrix is not set in function")
+                x <- NULL
+        }
+        get <- function() x
+        list(set = set, get = get)
 }
 
 
-## Write a short comment describing this function
+## Very straightforward function, extracts the matrix from previous list
 
 cacheSolve <- function(x, ...) {
+        data <- x$get()
+        inv <<- solve(data)
+        inv
         ## Return a matrix that is the inverse of 'x'
 }
